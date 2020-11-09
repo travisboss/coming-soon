9cfe66adadd757738b842bc8044fd972406a0fbf
Travis Boss <travisboss@gmail.com>
2020-11-09 15:42:53 -0600

    testing actions

--- src/common/forms/signup.js
+++ src/common/forms/signup.js
@@ -7,6 +7,7 @@ class EmailForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {value: ""};
+    // testing
     this.handleChange = this.handleChange.bind(this);
   }
   handleChange(event) {
