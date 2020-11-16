bea8164512393b27b5d1d0429fbfe9d81dd92cae
Travis Boss <travisboss@gmail.com>
2020-11-16 14:51:01 -0600

    testing form to google sheets

--- src/common/forms/signup.js
+++ src/common/forms/signup.js
@@ -1,24 +1,59 @@
 import React from 'react';
+import axios from 'axios'
 //import { LightCta } from '../buttons/cta.js';
 
+const formUrl = 'https://script.google.com/macros/s/AKfycbz4hT14AOvw48nUASRiw87m45XvIQ58Xu_GkBvzb92EHTcyMxni/exec'
 
 class EmailForm extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {value: ""};
-    // testing
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleChange(event) {
-    this.setState({value: event.target.value});
+  state = {
+    name: '',
+    email: '',
+    emailIsValid: false,
+    submitted: false
   }
 
+  updateValues = (e) => {
+    e.preventDefault()
+    this.setState({[e.target.name]: e.target.value})
+    this.validateEmail()
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const { name, email } = this.state
+
+    axios({
+      method: 'get',
+      url: `${formUrl}?name=${encodeURIComponent(name)}&email=${encodeURIComponent(email)}`
+    })
+
+
+    this.setState({submitted: true})
+  }
   render() {
     return (
-      <form>
-        <iframe title="signup form" src="https://docs.google.com/forms/d/e/1FAIpQLScHL4qQa-5RzDHtMtpb2C1A3KCebde-m4rMuWayUhwzGZ8hLg/viewform?embedded=true" width="700" height="600" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
-      </form>
+      <form
+      onChange={this.props.onChange}
+      onSubmit={this.props.onSubmit}
+      className='flex justify-around items-center'
+    >
+      <label className='mr3 avenir mt0 dark-gray'>
+        Your Email Address
+      </label>
+
+        <label className='mr3'>
+          <input
+            type='text'
+            name='email'
+            placeholder='email'
+            ref={node => (this.emailNode = node)}
+          />
+      </label>
+
+      <button className='f6 pa2 avenir dim'>
+        Submit
+      </button>
+    </form>
     )
   }
 }
